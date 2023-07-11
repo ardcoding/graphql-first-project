396cbee2297fa9557e9a3dbfd4e950519712a6b1
Ahmet Rüchan Dora <76576599+ardcoding@users.noreply.github.com>
2023-07-11 18:44:39 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const {ApolloServer, gql} = require('apollo-server');
 const {events, users, locations, participants} = require('./data.json')
 const {ApolloServerPluginLandingPageGraphQLPlayground} = require('apollo-server-core');
+const {nanoid} = require("nanoid");
 
 const typeDefs = gql `
 
@@ -53,9 +54,243 @@ const typeDefs = gql `
         participants : [Participant]
         participant(id: ID): Participant
     }
+    
+
+    input createEventInput{
+        title : String!
+        desc : String
+        date : String
+        from : String
+        to : String
+        location_id : Int
+        user_id : Int
+    }
+    input updateEventInput{
+        title : String
+        desc : String
+        date : String
+        from : String
+        to : String
+        location_id : Int
+        user_id : Int
+    }
+
+    input createLocationInput{
+        name : String!
+        desc : String
+        lat : Int
+        lng : Int
+    }
+    input updateLocationInput{
+        name : String
+        desc : String
+        lat : Int
+        lng : Int
+    }
+
+    input createUserInput{
+        username: String!
+        email: String
+    }
+    input updateUserInput{
+        username: String
+        email: String
+    }
+
+    input createParticipantInput{
+        user_id : ID!
+        event_id : ID!
+    }
+    input updateParticipantInput{
+        user_id : ID
+        event_id : ID
+    }
+
+    type deleteAllOutput{
+        count: Int
+    }
+
+    type Mutation{
+        createUser(data: createUserInput!): User!
+        updateUser(id:ID!, data: updateUserInput!): User!
+        deleteUser(id:ID!): User!
+        deleteAllUsers: deleteAllOutput!
+
+        createEvent(data: createEventInput!): Event!
+        updateEvent(id:ID!, data: updateEventInput!): Event!
+        deleteEvent(id:ID!): Event!
+        deleteAllEvents: deleteAllOutput!
+        
+        createLocation(data: createLocationInput!): Location!
+        updateLocation(id:ID!, data: updateLocationInput!): Location!
+        deleteLocation(id:ID!): Location!
+        deleteAllLocations: deleteAllOutput!
+
+        createParticipant(data: createParticipantInput!): Participant!
+        updateParticipant(id:ID!, data: updateParticipantInput!): Participant!
+        deleteParticipant(id:ID!): Participant!
+        deleteAllParticipants: deleteAllOutput!
+    }
+
 `;
 
 const resolvers = {
+    Mutation:{
+        createUser:(parent, {data}) => { 
+            const newUser = {
+                id: nanoid(), 
+                ...data,
+            }
+            users.push(newUser)
+            return newUser
+        },
+        updateUser:(parent, {id, data})=>{
+            const user = users.findIndex(user=> user.id == id)
+            if(user==-1){
+               throw new Error("Kullanıcı Bulunamadı")
+            }
+            const updatedUser  = users[user] = {
+                ...users[user],
+                ...data
+            }
+            return updatedUser
+        },
+        deleteUser:(parent, {id})=>{
+            const user = users.findIndex(user=> user.id == id)
+
+            if(user==-1){
+               throw new Error( "Kullanıcı Bulunamadı")
+            }
+
+            const deletedUser = users[user]
+            users.splice(user,1)
+            return deletedUser
+        },
+        deleteAllUsers: () => {
+            const length = users.length
+            users.splice(0,length)
+            return{
+                count: length,
+            }
+        },
+
+
+        createEvent:(parent, {data}) => { 
+            const newEvent = {
+                id: nanoid(), 
+                ...data,
+            }
+            events.push(newEvent)
+            return newEvent
+        },
+        updateEvent:(parent, {id, data})=>{
+            const event = events.findIndex(event=> event.id == id)
+            if(event==-1){
+               throw new Error( "Etkinlik Bulunamadı")
+            }
+            const updatedEvent  = events[event] = {
+                ...events[event],
+                ...data
+            }
+            return updatedEvent
+        },
+        deleteEvent:(parent, {id})=>{
+            const event = events.findIndex(event=> event.id == id)
+
+            if(event==-1){
+               throw new Error( "Etkinlik Bulunamadı")
+            }
+
+            const deletedEvent = events[event]
+            events.splice(event,1)
+            return deletedEvent
+        },
+        deleteAllEvents: () => {
+            const length = events.length
+            events.splice(0,length)
+            return{
+                count: length,
+            }
+        },
+
+
+        createLocation:(parent, {data}) => { 
+            const newLocation = {
+                id: nanoid(), 
+                ...data,
+            }
+            users.push(newLocation)
+            return newLocation
+        },
+        updateLocation:(parent, {id, data})=>{
+            const location = locations.findIndex(location=> location.id == id)
+            if(location==-1){
+               throw new Error("Etkinlik Yeri Bulunamadı")
+            }
+            const updatedLocation  = locations[location] = {
+                ...locations[location],
+                ...data
+            }
+            return updatedLocation
+        },
+        deleteLocation:(parent, {id})=>{
+            const location = locations.findIndex(location=> location.id == id)
+
+            if(location==-1){
+               throw new Error( "Etkinlik Yeri Bulunamadı")
+            }
+
+            const deletedLocation = locations[location]
+            locations.splice(location,1)
+            return deletedLocation
+        },
+        deleteAllLocations: () => {
+            const length = locations.length
+            locations.splice(0,length)
+            return{
+                count: length,
+            }
+        },
+
+
+        createParticipant:(parent, {data}) => { 
+            const newParticipant = {
+                id: nanoid(), 
+                ...data,
+            }
+            participants.push(newParticipant)
+            return newParticipant
+        },
+        updateParticipant:(parent, {id, data})=>{
+            const participant = participants.findIndex(participant=> participant.id == id)
+            if(participant==-1){
+               throw new Error( "Katılımcı Bulunamadı")
+            }
+            const updatedParticipant  = participants[participant] = {
+                ...participants[participant],
+                ...data
+            }
+            return updatedParticipant
+        },
+        deleteParticipant:(parent, {id})=>{
+            const participant = participants.findIndex(participant=> participant.id == id)
+
+            if(participant==-1){
+               throw new Error( "Katılımcı Bulunamadı")
+            }
+
+            const deletedParticipant = participants[participant]
+            participants.splice(participant,1)
+            return deletedParticipant
+        },
+        deleteAllParticipants: () => {
+            const length = participants.length
+            participants.splice(0,length)
+            return{
+                count: length,
+            }
+        },
+    },
     Query: {
       events: () => events,
       event:(parent,args) => events.find(event => (event.id).toString() === args.id),
